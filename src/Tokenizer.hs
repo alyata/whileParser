2406addf9588f48b168af94112e3a53f38b747a9
ar2519 <ar2519@ic.ac.uk>
2020-12-12 09:21:30 +0000

    implement tokenizer

---
+++ src/Tokenizer.hs
@@ -0,0 +1,21 @@
+module Tokenizer where
+
+import Text.Parsec
+
+whitespaceChars :: [Char]
+whitespaceChars = " \n"
+
+specialChars :: [Char]
+specialChars = "(+*)"
+
+whitespaceParser :: Parsec String st String
+whitespaceParser = many $ oneOf whitespaceChars
+
+-- a token is either a string of non-special/non-whitespace characters
+-- or exactly one special character
+tokenParser :: Parsec String st String
+tokenParser = (do ch <- oneOf specialChars; return [ch])
+             <|> (many1 $ noneOf (whitespaceChars ++ specialChars))
+
+tokenizer :: Parsec String st String
+tokenizer = whitespaceParser *> tokenParser
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,44 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module Main where
+
+-- the module to test
+import Tokenizer (whitespaceParser, tokenParser)
+
+-- other imports
+import Text.Parsec
+import Test.HUnit
+import Data.Functor.Identity
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do 
+  runTestTT tests
+  return ()
+
+tests :: Test
+tests = TestList [whitespaceParserTests, tokenParserTests]
+
+whitespaceParserTests :: Test
+whitespaceParserTests
+  = TestList $ fmap (uncurry (testParser whitespaceParser))
+      [("     wowwww", "     ")
+      ,("\n\n\nQWE" , "\n\n\n")
+      ]
+
+tokenParserTests :: Test
+tokenParserTests
+  = TestList $ fmap (uncurry (testParser tokenParser))
+      [("++++", "+") -- plus should be treated as 1 token on its own
+      ,("******", "*") -- star should be treated as 1 token on its own
+      ,("(aqwe)", "(") -- ( should be treated as 1 token on its own
+      ,(")()", ")") -- ) should be treated as 1 token on its own
+      ,("asdf qwer", "asdf") -- tokens are separated by whitespace
+      ,("asdf*qwer", "asdf") -- tokens are separated by special character
+      ]
+
+testParser :: (Eq o, Show o, Show i, Stream i Identity t) => 
+              Parsec i () o -> i -> o -> Test
+testParser parser input expected
+  = TestCase $ assertEqual ("parsing " ++ show input ++ ":")
+                           (parse parser "" input)
+                           (Right expected)
