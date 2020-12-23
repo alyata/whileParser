ff517a6f1cc0f90e7841092c54a5c05e9dc73e1e
ar2519 <ar2519@ic.ac.uk>
2020-12-23 15:43:29 +0000

    added C-like brackets to disambiguate commands

--- src/Command.hs
+++ src/Command.hs
@@ -5,8 +5,8 @@ import           Expr                (Expr, expr, variable)
 
 import           Boolean             (Boolean, boolean)
 import           Control.Applicative ((<**>))
-import           Text.Parsec         (ParseError, Parsec, chainr1, eof, parse,
-                                      spaces, (<?>), (<|>))
+import           Text.Parsec         (ParseError, Parsec, between, chainr1, eof,
+                                      parse, spaces, (<?>), (<|>))
 
 data Command =
   String ::=: Expr | If Boolean Command Command |
@@ -16,18 +16,24 @@ data Command =
 atom :: Parsec String st Command
 atom = (Skip <$ lexemeMatch "skip")
    <|> variable <**> ((::=:) <$ lexemeMatch ":=") <*> expr
+   <|> between (lexemeMatch "{" <?> "open bracket")
+               (lexemeMatch "}" <?> "close bracket")
+               command
 
 ifte :: Parsec String st Command
 ifte = (If <$ lexemeMatch "if") <*> boolean
-   <*> (lexemeMatch "then" *> command)
-   <*> (lexemeMatch "else" *> command)
+   <*> (lexemeMatch "then" *> singleCommand)
+   <*> (lexemeMatch "else" *> singleCommand)
 
 while :: Parsec String st Command
 while = (While <$ lexemeMatch "while") <*> boolean
-    <*> (lexemeMatch "do" *> command)
+    <*> (lexemeMatch "do" *> singleCommand)
+
+singleCommand :: Parsec String st Command
+singleCommand = ifte <|> while <|> atom
 
 command :: Parsec String st Command
-command = chainr1 (ifte <|> while <|> atom) ((:::) <$ lexemeMatch ";") <?> "command"
+command = chainr1 singleCommand ((:::) <$ lexemeMatch ";") <?> "command"
 
 parseCommand :: String -> Either ParseError Command
 parseCommand = parse (spaces *> command <* eof) ""
