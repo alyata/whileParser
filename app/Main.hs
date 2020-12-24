1334a424db4e8c7ac630b9d049f7d46f74e3130a
ar2519 <ar2519@ic.ac.uk>
2020-12-24 16:12:12 +0000

    implemented proof generation for commands

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,8 @@ module Main where
 
 import Expr (Expr(..), x)
 import Boolean (Boolean(..))
-import Latex (evalExpr, evalBoolean)
+import Command (Command(..))
+import Latex (evalExpr, evalBoolean, evalCommand)
 import Data.Map
 import Text.LaTeX
 import Text.LaTeX.Base.Render
@@ -10,6 +11,7 @@ import Text.LaTeX.Base.Render
 main :: IO ()
 main = do
   let st = fromList [("x", 2)]
-      proof1 = fst $ evalBoolean (T :|: (Not (x :>: 10))) st
-      proof2 = fst $ evalExpr ((x + 10) * 34 + (x * x * x * x)) st
-  renderFile "out.tex" $ proof1 <> proof2
+      --proof1 = fst $ evalBoolean (T :|: (Not (x :>: 10))) st
+      --proof2 = fst $ evalExpr ((x + 10) * 34 + (x * x * x * x)) st
+      proof3 = fst $ evalCommand (While (x :<: 20) ("x" ::=: (x + 1))) st
+  renderFile "out.tex" $ proof3
--- src/Latex.hs
+++ src/Latex.hs
@@ -6,6 +6,7 @@ module Latex where
 
 import Expr (Expr(..))
 import Boolean (Boolean(..))
+import Command (Command(..))
 
 import Text.LaTeX
 import Numeric.Natural
@@ -13,11 +14,37 @@ import Data.Map hiding (map)
 import Data.Maybe (fromJust)
 import Prelude hiding (lookup)
 import Text.LaTeX.Base.Syntax (TeXArg(FixArg), LaTeX(TeXComm, TeXCommS))
-import Text.LaTeX.Base.Class (LaTeXC, fromLaTeX, comm0, comm1, comm3)
+import Text.LaTeX.Base.Class (LaTeXC, fromLaTeX, comm0, comm1, comm2, comm3)
 import Data.List (intersperse)
 
 type Proof = LaTeX
 type State = Map String Natural
+
+evalCommand :: Command -> State -> (Proof, State)
+evalCommand Skip st = (druleCommand Skip st st mempty, st)
+evalCommand c@(id ::=: e) st = (druleCommand c st st' p, st')
+  where
+    (p, n) = evalExpr e st
+    st' = insert id n st
+evalCommand c@(If b c1 c2) st = (druleCommand c st st' (pb <> p), st')
+  where
+    (pb, b') = evalBoolean b st
+    (p, st') = evalCommand (if b' then c1 else c2) st
+evalCommand c@(c1 ::: c2) st = (druleCommand c st st'' (p1 <> p2), st'')
+  where
+    (p1, st') = evalCommand c1 st
+    (p2, st'') = evalCommand c2 st'
+evalCommand c@(While b c1) st = (druleCommand c st st'' (pb <> p <> pw), st'')
+  where
+    (pb, b') = evalBoolean b st
+    (p, st') = if b' then evalCommand c1 st else (mempty, st)
+    (pw, st'') = if b' then evalCommand c st' else (mempty, st)
+
+druleCommand :: Command -> State -> State -> Proof -> Proof
+druleCommand c st st' proof = drule proof (pre <> bsc <> post)
+  where
+    pre = config c st
+    post = texy st'
 
 -- | Evaluates an expression, returning the evaluation result and the proof tree
 -- | generated (as a LaTeX equation)
@@ -95,16 +122,19 @@ config val st = ang $ texy val <> fromString ", " <> texy st
 -- CUSTOM COMMANDS FROM TEMPLATE --
 
 drule :: LaTeXC l => l -> l -> l
-drule top bottom = comm3 "drule" "" top bottom
+drule = comm3 "drule" ""
 
 ang :: LaTeXC l => l -> l
-ang content = comm1 "ang" content
+ang = comm1 "ang"
 
 bse :: LaTeXC l => l
 bse = comm0 "bse"
 
 bsb :: LaTeXC l => l
 bsb = comm0 "bsb"
+
+bsc :: LaTeXC l => l
+bsc = comm0 "bsc"
 
 true :: LaTeXC l => l
 true = comm0 "true"
@@ -114,6 +144,12 @@ false = comm0 "false"
 
 neg :: LaTeXC l => l
 neg = comm0 "neg"
+
+ifthen :: LaTeXC l => l -> l -> l -> l
+ifthen = comm3 "ifthen"
+
+while :: LaTeXC l => l -> l -> l
+while = comm2 "while"
 
 -- LaTeX PRETTY PRINTERS --
 
@@ -140,6 +176,13 @@ instance Texy Boolean where
   texy (b1 :&: b2) = autoParens $ texy b1 `wedge` texy b2
   texy (b1 :|: b2) = autoParens $ texy b1 `vee` texy b2
   texy (Not b1) = autoParens $ neg <> texy b1
-  texy (e1 :=: e2) = autoParens $ texy e1 <> fromString "=" <> texy e2
-  texy (e1 :<: e2) = autoParens $ texy e1 <> fromString "<" <> texy e2
-  texy (e1 :>: e2) = autoParens $ texy e1 <> fromString ">" <> texy e2
+  texy (e1 :=: e2) = autoParens $ texy e1 <> "=" <> texy e2
+  texy (e1 :<: e2) = autoParens $ texy e1 <> "<" <> texy e2
+  texy (e1 :>: e2) = autoParens $ texy e1 <> ">" <> texy e2
+
+instance Texy Command where
+  texy (id ::=: e) = fromString id <> ":=" <> texy e
+  texy (If b c1 c2) = ifthen (texy b) (texy c1) (texy c2)
+  texy (c1 ::: c2) = texy c1 <> ";" <> texy c2
+  texy Skip = texttt "skip"
+  texy (While b c) = while (texy b) (texy c)
